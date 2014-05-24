a184e10c1e9c7bfa893d8ddc804f86519f1729aa
aicioara <primulandrei@yahoo.com>
2014-05-24 16:52:16 +0100

    injected stuff to page

--- index.php
+++ index.php
@@ -26,6 +26,9 @@
 				async: false,
 				success: function(result) {
 					console.log(result[4]);
+					$('#first').html(result[1].name + "-" + result[1].points);
+					$('#second').html(result[2].name + "-" + result[2].points);
+					$('#third').html(result[3].name + "-" + result[3].points);
 				}
 			});
 		}
